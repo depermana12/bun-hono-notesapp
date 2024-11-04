285e721669a2ea68227b8732c7d3d48c8740d35e
depermana12 <deddiapermana97@gmail.com>
2024-11-04 12:15:35 +0700

    init hono

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/hello", (c) => {
+  return c.json({
+    ok: true,
+    message: "hello from api",
+  });
+});
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
