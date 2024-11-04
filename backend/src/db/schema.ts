26ac15d4976e48e514b9afbc0f7f186ddf327974
depermana12 <deddiapermana97@gmail.com>
2024-11-04 20:47:11 +0700

    fix forget add password to user table

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -5,6 +5,7 @@ export const users = pgTable("users", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
+  password: text("password").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
 });
 
