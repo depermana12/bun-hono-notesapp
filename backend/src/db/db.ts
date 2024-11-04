94f743d46117ee541d9dfa66fc0361cf5150a28a
depermana12 <deddiapermana97@gmail.com>
2024-11-04 20:48:10 +0700

    fix conn string

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as dbSchema from "./schema";
 
-const db = drizzle(process.env.DATABASE_URL!, { schema: dbSchema });
+const db = drizzle("postgres://hononotes:hononotes@localhost:5432/hononotes", {
+  schema: dbSchema,
+});
 
 export default db;
