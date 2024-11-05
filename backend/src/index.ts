6caadf7c32d12a9ef61ebd0ae362fa721933c0be
depermana12 <deddiapermana97@gmail.com>
2024-11-05 14:21:57 +0700

    add handler for thrown HTTPException

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 import { Hono } from "hono";
 import user from "./routes/auth";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono().basePath("/api/v1");
 app.route("/auth", user);
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return c.json({ error: err.message }, err.status);
+  }
+  return c.json({ error: "oopss thats on us" }, 500);
+});
 
 export default {
   port: 3000,
