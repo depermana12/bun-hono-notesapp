33681afbb04a24dd08a53aac4929a928bd613180
depermana12 <deddiapermana97@gmail.com>
2024-11-05 16:50:52 +0700

    add note services

---
+++ backend/src/services/note.ts
@@ -0,0 +1,45 @@
+import { HTTPException } from "hono/http-exception";
+import NoteRepository from "../repositories/notes";
+
+type NoteData = {
+  title: string;
+  content: string;
+};
+
+class NoteService {
+  constructor(private noteRepository = new NoteRepository()) {}
+  public createNote(noteData: NoteData, userId: number) {
+    return this.noteRepository.create(noteData, userId);
+  }
+  public getNote(noteId: number, userId: number) {
+    const note = this.noteRepository.findById(noteId, userId);
+    if (!note) {
+      throw new HTTPException(404, { message: "note not found" });
+    }
+    return note;
+  }
+  public getNotes() {
+    return this.noteRepository.findAll();
+  }
+  public updateNote(
+    noteId: number,
+    title: string,
+    content: string,
+    userId: number,
+  ) {
+    const note = this.noteRepository.findById(noteId, userId);
+    if (!note) {
+      throw new HTTPException(404, { message: "note not found" });
+    }
+    return this.noteRepository.update(noteId, title, content, userId);
+  }
+  public deleteNote(noteId: number, userId: number) {
+    const note = this.noteRepository.findById(noteId, userId);
+    if (!note) {
+      throw new HTTPException(404, { message: "note not found" });
+    }
+    return this.noteRepository.delete(noteId, userId);
+  }
+}
+
+export default NoteService;
