8613d8f311f451171966fdc305a385dc097be108
depermana12 <deddiapermana97@gmail.com>
2024-11-05 16:50:27 +0700

    add note repository

--- backend/src/repositories/notes.ts
+++ backend/src/repositories/notes.ts
@@ -3,11 +3,12 @@ import db from "../db/db";
 import { notes } from "../db/schema";
 
 type Note = typeof notes.$inferSelect;
-type NewNote = typeof notes.$inferInsert;
-
+type NoteInsert = typeof notes.$inferInsert;
+type NewNote = Omit<NoteInsert, "userId">;
 class NoteRepository {
-  public async create(noteData: NewNote): Promise<Note> {
+  public async create(newNote: NewNote, userId: number): Promise<Note> {
     try {
+      const noteData = { ...newNote, userId };
       const rows = await db.insert(notes).values(noteData).returning();
       return rows[0];
     } catch (error: unknown) {
@@ -29,6 +30,36 @@ class NoteRepository {
     try {
       const rows = await db.select().from(notes);
       return rows;
+    } catch (error: unknown) {
+      throw new Error(error instanceof Error ? error.message : String(error));
+    }
+  }
+  public async update(
+    noteId: number,
+    title: string,
+    content: string,
+    userId: number,
+  ) {
+    try {
+      const rows = await db
+        .update(notes)
+        .set({ title, content, updated_at: new Date() })
+        .where(and(eq(notes.id, noteId), eq(notes.userId, userId)))
+        .returning();
+      return rows[0];
+    } catch (error: unknown) {
+      throw new Error(error instanceof Error ? error.message : String(error));
+      // drizzle not have support for error instance yet
+    }
+  }
+
+  public async delete(noteId: number, userId: number) {
+    try {
+      const rows = await db
+        .delete(notes)
+        .where(and(eq(notes.id, noteId), eq(notes.userId, userId)))
+        .returning({ id: notes.id, userId: notes.userId, title: notes.title });
+      return rows[0];
     } catch (error: unknown) {
       throw new Error(error instanceof Error ? error.message : String(error));
     }
